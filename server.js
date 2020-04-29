3e1e8509b070cac8b779b5e9844131f027d536a0
fionamiele <fionamiele@MacBook-Pro-de-Fiona.local>
2020-04-29 12:41:06 +0200

    turn

--- server.js
+++ server.js
@@ -20,11 +20,25 @@ io.on('connection', (socket) => {
 });
 
 let users = [];
+// qui dessine en ce mmt, valeur par défaut nulle
+let currentPlayer = null;
+let timeout;
+let words = ['apple', 'banana', 'coco'];
+
 function onConnection (socket) {
   socket.on('username', (username) => {
     console.log('Client name : ', username);
     socket.username = username;
-    users.push(socket);
+
+    if(users.length === 0) {
+      currentPlayer = socket;
+      timeout = clearTimeout(timeout);
+      // 5000 millisec
+      users.push(socket);
+      switchPlayer();
+    }else{
+      users.push(socket);
+    }
     sendUsers();
   });
 
@@ -36,16 +50,44 @@ function onConnection (socket) {
 
   socket.on('disconnect', () => {
     users = users.filter((user) => {
-      // ça veut dire = est-ce que le socket n'est pas égal à socket
+      // ça veut dire : est-ce que le socket n'est pas égal à socket
       return user !== socket;
     });
     sendUsers();
-
+    if (users.length === 0) {
+      timeout = clearTimeout(timeout);
+    }
   });
 }
 
 function sendUsers () {
   io.emit('users', users.map((user) => {
-    return user.username;
+    return {
+      username: user.username,
+      active: user === currentPlayer
+    };
   }))
+}
+
+function switchPlayer () {
+  // si plus pers ne joue on arrête
+  if(users.length === 0) return;
+
+  // passer au joueur suivant (chacun son tour)
+  const indexCurrentPlayer = users.indexOf(currentPlayer);
+  // ds un tableau de 3 valeurs c 0 1 2 et pas 1 2 3 c pr ça le calcul
+  // exemple
+  // users = [a, b, c]
+  // currentPlayer = a
+  currentPlayer = users[(indexCurrentPlayer + 1) % (users.length)];
+  // currentPlayer = users[3 % 3] -> users [0] -> a
+
+
+
+  sendUsers();
+  timeout = setTimeout(switchPlayer, 5000);
+
+  // mot aleatoire a recuperer dans le tableau le calcul c pr avoir une valeur ronde et pas 1.9999999...
+  currentPlayer.emit('word', words[Math.floor(words.length * Math.random())]);
+  io.emit('clear');
 }
